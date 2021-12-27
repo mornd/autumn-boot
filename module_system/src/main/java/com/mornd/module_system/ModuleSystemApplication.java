4e9991c2e27d8efca2bb0512486098310ed5d43f
mornd <1152229579@qq.com>
2021-12-27 13:07:58 +0800

    初始化项目

---
+++ module_system/src/main/java/com/mornd/module_system/ModuleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mornd.module_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleSystemApplication.class, args);
+    }
+
+}
