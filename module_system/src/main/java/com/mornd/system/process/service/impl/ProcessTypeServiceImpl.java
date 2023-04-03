bf42054770ef15e2470234d26a7dc7eddcc1a1be
yanglinchen <1152229579@qq.com>
2023-04-03 22:10:32 +0800

    git测试

--- module_system/src/main/java/com/mornd/system/process/service/impl/ProcessTypeServiceImpl.java
+++ module_system/src/main/java/com/mornd/system/process/service/impl/ProcessTypeServiceImpl.java
@@ -26,8 +26,6 @@ public class ProcessTypeServiceImpl
     public IPage<ProcessType> findTypeList(ProcessType processType) {
         IPage<ProcessType> page = new Page<>(processType.getPageNo(), processType.getPageSize());
         baseMapper.findTypeList(page, processType);
-
-        IPage<ProcessType> typeList = baseMapper.findTypeList(page, processType);
         return page;
     }
 
